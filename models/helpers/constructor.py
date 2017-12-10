f254f4726241f521ccdd93223939d0d10f0f1aa1
PrestonPhelan <preston.phelan@gmail.com>
2017-12-10 09:42:22 -0800

    Constructor docstring

--- models/helpers/constructor.py
+++ models/helpers/constructor.py
@@ -1,4 +1,5 @@
 def build_set_from_file(cls, sourcefile):
+    # Generic build set of objects from a file of objects
     items = {}
     with open(sourcefile, 'r') as f:
         for line in f:
@@ -11,7 +12,7 @@ def build_instance(self, attribute_list, data):
     # Takes in a list of column names and values for those columns,
     # saves as attributes on a class instance
     if len(attribute_list) != len(data):
-        raise "Lenghts do not match.  Expected attributes: %s, data: %s" % (len(attribute_list), len(data))
+        raise "Lengths do not match.  Expected attributes: %s, data: %s" % (len(attribute_list), len(data))
     for i in range(len(attribute_list)):
         command = build_command_for(attribute_list[i], data[i])
         exec(command)
